3b8b4301d7dcf5347409f79d76004413e4200620
Osama Ghaffar <116808680+OSAMAGHAFFARTKOJL@users.noreply.github.com>
2024-08-05 14:20:29 +0500

    Time: 39 ms (85.59%), Space: 17.3 MB (41.08%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        x=''.join(char for char in s if char.isalnum())
+        x=x.lower()
+        # return x == x[::-1]
+        l = 0
+        r = len(x)-1
+        while l<r:
+            if x[l] != x[r]:
+                return False
+            l+=1
+            r-=1
+        return True
