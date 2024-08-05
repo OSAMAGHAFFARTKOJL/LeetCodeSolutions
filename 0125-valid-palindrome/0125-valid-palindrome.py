b05be3ce9fad832275d0c22944a964b2dd535117
Osama Ghaffar <116808680+OSAMAGHAFFARTKOJL@users.noreply.github.com>
2024-08-05 15:54:17 +0500

    Time: 41 ms (77.6%), Space: 17.3 MB (36.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -7,13 +7,14 @@ class Solution:
                 x.append(char)
         x = "".join(x)
         x = x.lower()
+        return x == x[::-1]
         
-        l = 0
-        r = len(x)-1
-        while l < r :
-            if x[l] != x[r]:
-                return False
-            l += 1
-            r -= 1
-        return True
+        # l = 0
+        # r = len(x)-1
+        # while l < r :
+        #     if x[l] != x[r]:
+        #         return False
+        #     l += 1
+        #     r -= 1
+        # return True
 
