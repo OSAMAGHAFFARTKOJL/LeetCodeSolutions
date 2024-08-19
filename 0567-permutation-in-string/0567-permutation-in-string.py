21d4051e3378e17dcba1dd51f894934bcf12ae41
Osama Ghaffar <116808680+OSAMAGHAFFARTKOJL@users.noreply.github.com>
2024-08-19 15:49:33 +0500

    Time: 90 ms (37.96%), Space: 16.7 MB (18.24%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -9,47 +9,42 @@ class Solution:
         #     else:
         #         count = 0
         # return False
-        l = 0 
-        s1 = sorted(s1)
-        for r in range(len(s1),len(s2)+1):
-            x = s2[l:r]
-            x = sorted(x)
-            if x == s1:
-                return True
-            l+= 1
-        return False
-
-
-
-        # if len(s1) > len(s2):
-        #     return False
-
-        # s1Count, s2Count = [0] * 26, [0] * 26
-        # for i in range(len(s1)):
-        #     s1Count[ord(s1[i]) - ord("a")] += 1
-        #     s2Count[ord(s2[i]) - ord("a")] += 1
-
-        # matches = 0
-        # for i in range(26):
-        #     matches += 1 if s1Count[i] == s2Count[i] else 0
-
-        # l = 0
-        # for r in range(len(s1), len(s2)):
-        #     if matches == 26:
+        # l = 0 
+        # s1 = sorted(s1)
+        # for r in range(len(s1),len(s2)+1):
+        #     x = s2[l:r]
+        #     x = sorted(x)
+        #     if x == s1:
         #         return True
+        #     l+= 1
+        # return False
 
-        #     index = ord(s2[r]) - ord("a")
-        #     s2Count[index] += 1
-        #     if s1Count[index] == s2Count[index]:
-        #         matches += 1
-        #     elif s1Count[index] + 1 == s2Count[index]:
-        #         matches -= 1
 
-        #     index = ord(s2[l]) - ord("a")
-        #     s2Count[index] -= 1
-        #     if s1Count[index] == s2Count[index]:
-        #         matches += 1
-        #     elif s1Count[index] - 1 == s2Count[index]:
-        #         matches -= 1
-        #     l += 1
-        # return matches == 26
+
+        len_s1 = len(s1)
+        len_s2 = len(s2)
+        
+        if len_s1 > len_s2:
+            return False
+
+        s1_count = Counter(s1)
+        window_count = Counter(s2[:len_s1])
+        
+        if s1_count == window_count:
+            return True
+        
+        for i in range(len_s1, len_s2):
+            start_char = s2[i - len_s1]
+            new_char = s2[i]
+            
+            window_count[new_char] += 1
+            
+            if window_count[start_char] == 1:
+                del window_count[start_char]
+            else:
+                window_count[start_char] -= 1
+            
+            if window_count == s1_count:
+                return True
+        
+        return False
