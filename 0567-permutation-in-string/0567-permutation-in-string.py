1c278fffcd9616f81bcbf31e24386fd5580cf714
Osama Ghaffar <116808680+OSAMAGHAFFARTKOJL@users.noreply.github.com>
2024-08-19 15:54:27 +0500

    Time: 3115 ms (9.85%), Space: 16.7 MB (56.59%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -9,42 +9,43 @@ class Solution:
         #     else:
         #         count = 0
         # return False
-        # l = 0 
-        # s1 = sorted(s1)
-        # for r in range(len(s1),len(s2)+1):
-        #     x = s2[l:r]
-        #     x = sorted(x)
-        #     if x == s1:
-        #         return True
-        #     l+= 1
-        # return False
-
-
-
-        len_s1 = len(s1)
-        len_s2 = len(s2)
-        
-        if len_s1 > len_s2:
-            return False
-
-        s1_count = Counter(s1)
-        window_count = Counter(s2[:len_s1])
-        
-        if s1_count == window_count:
-            return True
-        
-        for i in range(len_s1, len_s2):
-            start_char = s2[i - len_s1]
-            new_char = s2[i]
-            
-            window_count[new_char] += 1
-            
-            if window_count[start_char] == 1:
-                del window_count[start_char]
-            else:
-                window_count[start_char] -= 1
-            
-            if window_count == s1_count:
+        l = 0 
+        s1 = sorted(s1)
+        for r in range(len(s1),len(s2)+1):
+            x = s2[l:r]
+            x = sorted(x)
+            if x == s1:
                 return True
-        
+            l+= 1
         return False
+
+
+
+        # len_s1 = len(s1)
+        # len_s2 = len(s2)
+        
+        # if len_s1 > len_s2:
+        #     return False
+
+        # s1_count = Counter(s1)
+        # window_count = Counter(s2[:len_s1])
+        # print(s1_count)
+        
+        # if s1_count == window_count:
+        #     return True
+        
+        # for i in range(len_s1, len_s2):
+        #     start_char = s2[i - len_s1]
+        #     new_char = s2[i]
+            
+        #     window_count[new_char] += 1
+            
+        #     if window_count[start_char] == 1:
+        #         del window_count[start_char]
+        #     else:
+        #         window_count[start_char] -= 1
+            
+        #     if window_count == s1_count:
+        #         return True
+        
+        # return False
