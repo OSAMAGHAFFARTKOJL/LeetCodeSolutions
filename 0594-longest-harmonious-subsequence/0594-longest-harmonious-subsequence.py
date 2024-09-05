8212a8f61f68287a84d782565cf624b73ea8c080
Osama Ghaffar <116808680+OSAMAGHAFFARTKOJL@users.noreply.github.com>
2024-09-05 15:29:42 +0500

    Time: 234 ms (68.55%), Space: 18.2 MB (89%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -1,14 +1,22 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-
+        hsh = {}
         ans = 0
+
         for i in nums:
-            x = nums.count(i)
-            y = nums.count(i+1)
-            if x and y:
-                z=x+y
-                if z >ans:
-                    ans = z
+            if i in hsh:
+                hsh[i] +=1
+            else:
+                hsh[i] = 1
+
+        for i in hsh:
+            x = hsh[i]
+            if i+1 in hsh: 
+                y = hsh[i+1]
+                if x and y:
+                    z=x+y
+                    if z >ans:
+                        ans = z
         return ans 
 
 
