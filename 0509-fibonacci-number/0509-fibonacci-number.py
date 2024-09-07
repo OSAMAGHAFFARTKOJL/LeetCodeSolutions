841cc036336037e8e9436f733bc43fb5a7556951
Osama Ghaffar <116808680+OSAMAGHAFFARTKOJL@users.noreply.github.com>
2024-09-07 15:40:18 +0500

    Time: 34 ms (74.31%), Space: 16.4 MB (58.54%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -10,17 +10,34 @@
 
 
 #Solution with memoization
+# class Solution:
+#     def __init__(self):
+#         self.memo = {}
+#     def fib(self, n: int) -> int:
+#         if n in self.memo:
+#             return self.memo[n]
+#         if n == 0:
+#             return 0
+#         if n<=2 :
+#             return 1
+#         result = self.fib(n-1)+self.fib(n-2)
+#         self.memo[n] = result
+#         return result
+        
+
+
+#improve memoization approach
+
 class Solution:
     def __init__(self):
-        self.memo = {}
+        self.memo = {1: 1, 2: 1,0:0}  # Base cases
+
     def fib(self, n: int) -> int:
         if n in self.memo:
-            return self.memo[n]
-        if n == 0:
-            return 0
-        if n<=2 :
-            return 1
-        result = self.fib(n-1)+self.fib(n-2)
-        self.memo[n] = result
-        return result
+            return self.memo[n]  # Return cached result
         
+        # Calculate Fibonacci iteratively and store in memo
+        for i in range(3, n + 1):
+            self.memo[i] = self.memo[i - 1] + self.memo[i - 2]
+        
+        return self.memo[n]
