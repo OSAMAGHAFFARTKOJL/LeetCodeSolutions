d09ec54a505269ab5be0a4df080a767937448466
Osama Ghaffar <116808680+OSAMAGHAFFARTKOJL@users.noreply.github.com>
2024-09-07 15:30:17 +0500

    Time: 509 ms (5.09%), Space: 16.5 MB (58.54%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,9 +1,23 @@
+#Solution without memoization
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         if n == 0:
+#             return 0
+#         if n<=2 :
+#             return 1
+#         result = self.fib(n-1)+self.fib(n-2)
+#         return result
+
+
+#Solution with memoization
 class Solution:
     def fib(self, n: int) -> int:
+        memo = {}
         if n == 0:
             return 0
         if n<=2 :
             return 1
         result = self.fib(n-1)+self.fib(n-2)
+        memo[n] = result
         return result
         
